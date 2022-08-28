a5926e63ae47d028e9c66f5058b9006c4b9f5089
Rodrigo <rodrigo@irestify.com>
2022-08-28 09:27:26 -0300

    Changes ...

---
+++ models/prod/faturamento_ano.sql
@@ -0,0 +1,16 @@
+
+with faturamento_ano as (
+
+select
+  DATE_PART('year', purchased_at ) AS "Ano",
+  sum(ap.price) as "Faturameto"
+from public.airbyte_purchases pur inner join airbyte_products ap
+on pur.product_id = ap.id
+group by 1
+order by 1;
+
+),
+
+
+
+select * from faturamento_ano
---
+++ models/prod/faturamento_modelo.sql
@@ -0,0 +1,13 @@
+
+with faturamento_model as (
+
+select pro.model as "Model"
+      ,sum(pro.price) as "Faturamento"
+from public.airbyte_purchases pur inner join airbyte_products pro
+on pur.product_id = pro.id
+group by pro.model
+order by 2 desc
+
+),
+
+select * from faturamento_model
