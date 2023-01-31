3a5c9d660d60e7bd3fb625a29758fe146a0d108b
pbisolucoes <103379961+pbisolucoes@users.noreply.github.com>
2023-01-31 13:46:52 -0300

    Update dw_faturamento_ano.sql

--- models/prod/dw_faturamento_ano.sql
+++ models/prod/dw_faturamento_ano.sql
@@ -3,7 +3,7 @@ with dw_faturamento_ano as (
 
     select DATE_PART('year', purchased_at ) AS "Ano"
         ,sum(ap.price) as "Faturameto"
-    from public.airbyte_purchases pur inner join airbyte_products ap
+    from airbyte_purchases pur inner join airbyte_products ap
     on pur.product_id = ap.id
     group by 1
     order by 1
