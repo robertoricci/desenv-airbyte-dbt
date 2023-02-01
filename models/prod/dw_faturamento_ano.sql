cd6a01e6de9cb91105bbb78aea9724b1fd8079e5
pbisolucoes <103379961+pbisolucoes@users.noreply.github.com>
2023-02-01 15:03:40 -0300

    Update dw_faturamento_ano.sql

--- models/prod/dw_faturamento_ano.sql
+++ models/prod/dw_faturamento_ano.sql
@@ -1,14 +1,10 @@
-
 with dw_faturamento_ano as (
-
-    select DATE_PART('year', purchased_at ) AS "Ano"
-        ,sum(ap.price) as "Faturameto"
+    select DATE_PART('year', TO_DATE( purchased_at) ) AS "Ano"
+        ,sum(ap.price) as "Faturamento"
     from dbt.purchases pur inner join dbt.products ap
     on pur.product_id = ap.id
     group by 1
     order by 1
 
 )
-
-
 select * from dw_faturamento_ano
