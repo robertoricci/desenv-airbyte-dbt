cd57b1960cdbaf7704f90f4035c2fcd110b361e2
pbisolucoes <103379961+pbisolucoes@users.noreply.github.com>
2023-02-01 14:15:51 -0300

    Update dw_faturamento_modelo.sql

--- models/prod/dw_faturamento_modelo.sql
+++ models/prod/dw_faturamento_modelo.sql
@@ -3,7 +3,7 @@ with dw_faturamento_model as (
 
     select pro.model as "Model"
           ,sum(pro.price) as "Faturamento"
-    from dbt.airbyte_purchases pur inner join dbt.airbyte_products pro
+    from dbt.purchases pur inner join dbt.products pro
     on pur.product_id = pro.id
     group by pro.model
     order by 2 desc
